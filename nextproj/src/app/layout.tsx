9ec210c693a70f237f2970bea0edf74f136facf8
ashley <25075152@edgehill.ac.uk>
2023-09-27 14:48:37 +0100

    fixed 2 bugs caused by props in navigation

--- nextproj/src/app/layout.tsx
+++ nextproj/src/app/layout.tsx
@@ -21,7 +21,9 @@ export default function RootLayout({children,}: {children: React.ReactNode}) {
     <html lang="en" className={roboto.className}>  
       <BodyStyled>
         <Heading/>
-        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+        <StyledComponentsRegistry>
+          {children}
+          </StyledComponentsRegistry>
         <NavBar/>
       </BodyStyled>
     </html>
